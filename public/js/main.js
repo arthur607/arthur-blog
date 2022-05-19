2170418206ea468a8f032e811c06d6609f963ca7
arthur.garcia <arthurrodriguesg1@gmail.com>
2022-05-19 10:13:32 -0300

    ajustes finais

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,42 @@
 let toggle = document.querySelector("#header .toggle-button");
 let collapse = document.querySelectorAll("#header .collapse");
 
-toggle.addEventListener('click' , function(){
+toggle.addEventListener('click', function () {
     collapse.forEach(col => col.classList.toggle("collapse-toggle"));
 })
+
+
+const blogSection = document.querySelector(".blogs-section");
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", `http://localhost:8080/buscar`, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.addEventListener("load", function () {
+
+    if (xhr.status != 200) {
+        console.log("erro na requisição");
+    }
+    const articleResponseList = JSON.parse(xhr.responseText);
+
+    console.log(articleResponseList);
+    articleResponseList.forEach(blog => {
+        if (blog.title != decodeURI(location.pathname.split("/").pop())) {
+            createBlog(blog);
+        }
+    })
+
+    
+   
+})
+
+const createBlog = (blog) => {
+    blogSection.innerHTML += `
+<div class="blog-card">
+  <img src="${blog.bannerImage}" class="blog-image" alt="">
+  <h1 class="blog-title">${blog.title.substring(0, 100) + '...'}</h1>
+  <p class="blog-overview">${blog.article.substring(0, 200) + '...'}</p>
+  <a href="/${blog.titleWithKey}" class="btn dark">read</a>
+</div>
+`;
+}
+xhr.send();
